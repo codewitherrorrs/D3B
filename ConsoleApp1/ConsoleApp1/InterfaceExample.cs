a619ba6bbd01ec43db1ee14d9e76572b82e67efa
“codewitherrors” <codewitherrors1@gmail.com>
2023-07-06 11:51:04 +0530

    {Sagar} Add : Interface Example

---
+++ ConsoleApp1/ConsoleApp1/InterfaceExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface InterfaceExample
+    {
+        void add(int a, int b);
+    }
+    public interface Interface
+    {
+        void Add(int b, int a);
+    }
+    public class Child : InterfaceExample, Interface
+    {
+        public void add(int a, int b) 
+        {
+            Console.WriteLine($"The Addition is {a+b}");
+        }
+
+        public void Add(int b,int a)
+        {
+            Console.WriteLine($"The sum is {a+b}");
+        }
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,14 @@
     {
         public static void Main(String[] args)
         {
-            Construct c1 = new Construct();
+            /*Construct c1 = new Construct();
             Construct c2 = new Construct("Sagar Shahu");
-            c2.display();
+            c2.display();*/
+
+            InterfaceExample inter = new Child();
+            Interface interOne = new Child();
+            inter.add(54, 5);
+            interOne.Add(54, 56);
         }
     }
 }
