b9680fdded7f283f89bdd5ce1a0b8f2bb4ab5892
“codewitherrors” <codewitherrors1@gmail.com>
2023-07-06 11:39:23 +0530

    {sagar} Add : Types Of Constructor Problem

---
+++ ConsoleApp1/ConsoleApp1/Construct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Construct
+    {
+        string name;
+        //Simple Constructor
+        public Construct() 
+        {
+            Console.WriteLine("These is Simple Constructor");
+        }
+        //Parameterized Constuctor
+        public Construct(string name)
+        {
+            this.name = name;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Name : {name}");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Construct c1 = new Construct();
+            Construct c2 = new Construct("Sagar Shahu");
+            c2.display();
+        }
+    }
+}
